0b2d3157eef58f499963d04417e5b1d2f07147af
elparo290 <parovelfilippo1@gmail.com>
2022-05-06 14:42:24 +0200

    Prova javadoc

--- Progetto/src/Model/UtenteGenerico.java
+++ Progetto/src/Model/UtenteGenerico.java
@@ -1,9 +1,25 @@
 package Model;
 
+/**
+ * Classe di un utente generico che non si deve registrere 
+ * @author Parovel
+ * @author Di gati
+ * @version 1.0
+ * 
+ */
+
 public class UtenteGenerico {
 	private String nome;
 	private String cognome;
 	private String telefono;
+
+	/**
+	 * Metodo per creare l'utente generico.
+	 * il numeor di telefono non viene passato perchè glielo assegno io
+	 * @param nome nome utente generico
+	 * @param cognome cognome utente generico
+	 * @param telefono telefono utente generico
+	 */
 	
 	public UtenteGenerico(String nome,String cognome,String telefono)
 	{
@@ -12,10 +28,15 @@ public class UtenteGenerico {
 		this.setTelefono(telefono);
 	}
 
+	/**
+	 * 
+	 * @return Ritorna il nome dell'utente generico
+	 */
+	
 	public String getNome() {
 		return nome;
 	}
-
+	
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
