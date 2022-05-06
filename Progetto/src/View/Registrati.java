e13539621f556b406a5b9a63b3d758606c2c24dd
elparo290 <parovelfilippo1@gmail.com>
2022-05-06 16:35:08 +0200

    ciao

--- Progetto/src/View/Registrati.java
+++ Progetto/src/View/Registrati.java
@@ -4,13 +4,24 @@ import javax.swing.JPanel;
 import java.awt.Color;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.util.Properties;
+
 import javax.swing.JTextField;
+
+import org.jdatepicker.impl.JDatePanelImpl;
+import org.jdatepicker.impl.JDatePickerImpl;
+import org.jdatepicker.impl.UtilDateModel;
+
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+import javax.swing.JRadioButton;
 
 public class Registrati extends JPanel {
 	private JTextField textField;
 	private JTextField textField_1;
 	private JTextField textField_2;
 	private JTextField textField_3;
+	private JPasswordField passwordField;
 
 	/**
 	 * Create the panel.
@@ -21,44 +32,85 @@ public class Registrati extends JPanel {
 		
 		JLabel lblNewLabel = new JLabel("NOME:");
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblNewLabel.setBounds(47, 133, 97, 13);
+		lblNewLabel.setBounds(46, 119, 97, 13);
 		add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("COGNOME:");
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblNewLabel_1.setBounds(14, 171, 180, 13);
+		lblNewLabel_1.setBounds(14, 143, 180, 13);
 		add(lblNewLabel_1);
 		
 		textField = new JTextField();
-		textField.setBounds(98, 132, 96, 19);
+		textField.setBounds(98, 117, 96, 19);
 		add(textField);
 		textField.setColumns(10);
 		
 		textField_1 = new JTextField();
-		textField_1.setBounds(98, 170, 96, 19);
+		textField_1.setBounds(98, 143, 96, 19);
 		add(textField_1);
 		textField_1.setColumns(10);
 		
 		JLabel lblNewLabel_2 = new JLabel("EMAIL:");
 		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblNewLabel_2.setBounds(46, 210, 110, 13);
+		lblNewLabel_2.setBounds(246, 143, 110, 13);
 		add(lblNewLabel_2);
 		
 		textField_2 = new JTextField();
-		textField_2.setBounds(98, 209, 96, 19);
+		textField_2.setBounds(296, 117, 144, 19);
 		add(textField_2);
 		textField_2.setColumns(10);
 		
 		JLabel lblNewLabel_3 = new JLabel("TELEFONO:");
 		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblNewLabel_3.setBounds(14, 248, 98, 13);
+		lblNewLabel_3.setBounds(214, 119, 98, 13);
 		add(lblNewLabel_3);
 		
 		textField_3 = new JTextField();
-		textField_3.setBounds(98, 247, 96, 19);
+		textField_3.setBounds(296, 141, 144, 19);
 		add(textField_3);
 		textField_3.setColumns(10);
-
+		
+		JLabel lblNewLabel_4 = new JLabel("PASSWORD;");
+		lblNewLabel_4.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNewLabel_4.setBounds(214, 167, 98, 14);
+		add(lblNewLabel_4);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(296, 167, 144, 20);
+		add(passwordField);
+		
+		JButton btnNewButton = new JButton("INVIA");
+		btnNewButton.setBounds(183, 266, 89, 23);
+		add(btnNewButton);
+		
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("Amministratore");
+		rdbtnNewRadioButton.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		rdbtnNewRadioButton.setBackground(Color.MAGENTA);
+		rdbtnNewRadioButton.setBounds(317, 194, 123, 23);
+		add(rdbtnNewRadioButton);
+		
+		JLabel lblNewLabel_5 = new JLabel("DATA DI NASCITA;");
+		lblNewLabel_5.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNewLabel_5.setBounds(14, 183, 167, 14);
+		add(lblNewLabel_5);
+		
+		JLabel lblNewLabel_6 = new JLabel("PAROGATI DISCO");
+		lblNewLabel_6.setForeground(Color.CYAN);
+		lblNewLabel_6.setFont(new Font("Segoe Print", Font.BOLD | Font.ITALIC, 30));
+		lblNewLabel_6.setBounds(77, 34, 322, 54);
+		add(lblNewLabel_6);
+		
+		UtilDateModel model = new UtilDateModel();
+		Properties p = new Properties();
+		p.put("text.today", "Oggi");
+		p.put("text.month", "Mese");
+		p.put("text.year", "Anno");
+		JDatePanelImpl datePanel = new JDatePanelImpl(model, p);
+		JDatePickerImpl datePicker = new JDatePickerImpl(datePanel, new DateLabelFormatter());
+        datePicker.setTextEditable(true);
+        datePicker.setShowYearButtons(true);
+		datePicker.setBounds(14, 208, 224, 21);
+		add(datePicker);
+		
 	}
-
 }
